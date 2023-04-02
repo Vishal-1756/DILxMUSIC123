008768abd441b4cbc7a5897eab0694a5cc7091bf
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-03 02:29:15 +0530

    Update bot.py

--- DILX/core/bot.py
+++ DILX/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class DilXBot(Client):
+class DILXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
