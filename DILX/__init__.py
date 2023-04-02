f2ef63984d39c36212e1dd8cdc30b29d494b2613
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-03 00:46:50 +0530

    Add files via upload

---
+++ DILX/__init__.py
@@ -0,0 +1,41 @@
+from DILX.core.bot import AnonXBot
+from DILX.core.dir import dirr
+from DILX.core.git import git
+from DILX.core.userbot import Userbot
+from DILX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = DILXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
