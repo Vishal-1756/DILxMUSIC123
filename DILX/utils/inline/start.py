d9f7d642c597f05c99b9574ca3be677c8cf37558
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-03 01:00:42 +0530

    Add files via upload

---
+++ DILX/utils/inline/start.py
@@ -0,0 +1,62 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from DILX import app
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🥺 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ [❤‍🔥ᴅɪʟ❤‍🔥] 🥺",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="❣️ʜᴇʟᴩ❣️",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="❤‍🔥sᴇᴛᴛɪɴɢs❤‍🔥", callback_data="settings_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="💝ᴍᴀɪɴᴛᴀɪɴᴇʀ[ᴅɪʟ]💝", user_id=OWNER),
+            InlineKeyboardButton(
+                text="🥰sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]🥰", url=config.SUPPORT_GROUP
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🥺 ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜ ɢᴇʏ [💞ᴅɪʟ💞]🥺",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="🥺ʜᴇʟᴩ🥺", callback_data="settings_back_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="💝ᴍᴀɪɴᴛᴀɪɴᴇʀ[ᴅɪʟ]💝", user_id=OWNER),
+            InlineKeyboardButton(
+                text="sᴜᴩᴩᴏʀᴛ[ᴀɪᴍ]", url=config.SUPPORT_GROUP
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                    text="🤨sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ🤨", url=config.UPSTREAM_REPO
+                )
+        ],
+     ]
+    return buttons
